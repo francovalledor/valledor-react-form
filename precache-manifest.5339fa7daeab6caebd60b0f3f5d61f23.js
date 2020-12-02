7e0f4046d06015184314628517f25229a0786496
Franco Valledor <francovalledor@gmail.com>
2020-12-02 16:49:53 -0300

    Updates

---
+++ precache-manifest.5339fa7daeab6caebd60b0f3f5d61f23.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba77d50c49369edb04e6fff59ecbd5c5",
+    "url": "./index.html"
+  },
+  {
+    "revision": "44ed88cba17c884ec557",
+    "url": "./static/css/2.c9e7500d.chunk.css"
+  },
+  {
+    "revision": "6b41f5eb59c89a20d722",
+    "url": "./static/css/main.1b9bcb54.chunk.css"
+  },
+  {
+    "revision": "44ed88cba17c884ec557",
+    "url": "./static/js/2.10bf631e.chunk.js"
+  },
+  {
+    "revision": "eb745704df7294d386459c0622d6566d",
+    "url": "./static/js/2.10bf631e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6b41f5eb59c89a20d722",
+    "url": "./static/js/main.990e4117.chunk.js"
+  },
+  {
+    "revision": "84f37aea000015f3ff77",
+    "url": "./static/js/runtime-main.b90084ed.js"
+  }
+]);
